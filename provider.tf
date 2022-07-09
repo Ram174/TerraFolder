f79d28083a118a5f0303549de737b449df4f59b6
Rama Krishna <83940508+Ram174@users.noreply.github.com>
2022-07-09 09:08:41 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -8,11 +8,11 @@ provider "azurerm" {
     tenant_id = var.tenant_id
   
 }
-terraform {
-  backend "azurerm"{
-    # resource_group_name  = var.resource_group_name
-    # storage_account_name = var.storage_account_name
-    # container_name       = var.container_name
-    # key                  = var.key
-  }
-}
+# terraform {
+#   backend "azurerm"{
+#     # resource_group_name  = var.resource_group_name
+#     # storage_account_name = var.storage_account_name
+#     # container_name       = var.container_name
+#     # key                  = var.key
+#   }
+# }
